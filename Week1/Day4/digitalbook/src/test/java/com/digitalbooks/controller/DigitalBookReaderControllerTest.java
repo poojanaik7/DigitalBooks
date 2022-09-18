93b81501e919d7fba6b67466eafba13697723ec1
unknown <cogjava3192@iiht.tech>
2022-09-19 01:55:54 +0530

    Junit and api changes

--- Week1/Day4/digitalbook/src/test/java/com/digitalbooks/controller/DigitalBookReaderControllerTest.java
+++ Week1/Day4/digitalbook/src/test/java/com/digitalbooks/controller/DigitalBookReaderControllerTest.java
@@ -1,5 +1,69 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.digitalbooks.controller;
 
-class DigitalBookReaderControllerTest {
+import com.digitalbooks.entity.Payment;
+import com.digitalbooks.model.BookResponse;
+import com.digitalbooks.model.PaymentModel;
+import com.digitalbooks.model.PaymentRequest;
+import com.digitalbooks.service.DigitalBookService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DigitalBookReaderControllerTest {
+
+    @Mock
+    DigitalBookService userService;
+
+    @InjectMocks
+    DigitalBookReaderController digitalBookReaderController;
+
+    @Test
+    public void searchBook() {
+        BookResponse bookResponse = new BookResponse("title","publisher",new Date(),"category",20l,"author","content",true);
+        List<BookResponse> bookResponseList = new ArrayList<>();
+        bookResponseList.add(bookResponse);
+        Mockito.when(userService.getBookDetails("category","author",20l,"publisher")).thenReturn(bookResponseList);
+        digitalBookReaderController.searchBook("category","author",20l,"publisher");
+    }
+
+    @Test
+    public void buyBook() throws Exception{
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setBookId(123);
+        paymentRequest.setUserId(123);
+        Mockito.when(userService.buyBook(paymentRequest)).thenReturn(new Payment(new Date(),null,null));
+        digitalBookReaderController.buyBook(paymentRequest);
+    }
+
+    @Test
+    public void readPaymentDetails() {
+        PaymentModel paymentModel = new PaymentModel(12l,new Date(),"abc",123);
+        List<PaymentModel> paymentModels = new ArrayList<>();
+        paymentModels.add(paymentModel);
+        Mockito.when(userService.getPaymentDetails(123)).thenReturn(paymentModels);
+        digitalBookReaderController.readPaymentDetails(123);
+    }
+
+    @Test
+    public void readContent() {
+        Mockito.when(userService.readContent(123)).thenReturn("content");
+        digitalBookReaderController.readContent(123);
+    }
+
+    @Test
+    public void getPaymentById() {
+        PaymentModel paymentModel = new PaymentModel(12l,new Date(),"abc",123);
+        List<PaymentModel> paymentModels = new ArrayList<>();
+        paymentModels.add(paymentModel);
+        Mockito.when(userService.getPaymentDetailsById(12l)).thenReturn(paymentModel);
+        digitalBookReaderController.getPaymentById(123l);
+    }
 }
--- Week1/Day4/digitalbook/src/test/java/com/digitalbooks/service/DigitalBookServiceTest.java
+++ Week1/Day4/digitalbook/src/test/java/com/digitalbooks/service/DigitalBookServiceTest.java
@@ -1,5 +1,175 @@
+package com.digitalbooks.service;
+
+import com.digitalbooks.entity.Book;
+import com.digitalbooks.entity.Payment;
+import com.digitalbooks.entity.User;
+import com.digitalbooks.exception.DigitalBookException;
+import com.digitalbooks.exception.ResultNotFoundException;
+import com.digitalbooks.model.BookRequest;
+import com.digitalbooks.model.BookResponse;
+import com.digitalbooks.model.PaymentModel;
+import com.digitalbooks.model.PaymentRequest;
+import com.digitalbooks.repository.DigitalBookRepository;
+import com.digitalbooks.repository.PaymentRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class DigitalBookServiceTest {
+@RunWith(MockitoJUnitRunner.class)
 
+public class DigitalBookServiceTest {
+
+    @InjectMocks
+    DigitalBookService digitalBookService;
+
+    @Mock
+    DigitalBookRepository digitalBookRepository;
+
+    @Mock
+    PaymentRepository paymentRepository;
+
+    @Test
+    public void getBookDetails() {
+        User user = new User();
+        user.setId(123);
+        user.setRoles(null);
+        user.setEmail("abc");
+        user.setUsername("abc");
+        Book book = new Book("title","publisher",new Date(),"category",20l,true,user,"content");
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(book);
+        Mockito.when(digitalBookRepository.getBookDetails("category","author",20l,"publisher")).thenReturn(bookList);
+        List<BookResponse> responses = digitalBookService.getBookDetails("category","author",20l,"publisher");
+        Assert.assertNotNull(responses);
+    }
+
+    @Test(expected = ResultNotFoundException.class)
+    public void getBookDetailsException() {
+        List<Book> bookList = new ArrayList<>();
+        Mockito.when(digitalBookRepository.getBookDetails("category","author",20l,"publisher")).thenReturn(bookList);
+        List<BookResponse> responses = digitalBookService.getBookDetails("category","author",20l,"publisher");
+        Assert.assertNotNull(responses);
+    }
+
+    @Test
+    public void createBook() throws Exception {
+        User user = new User();
+        user.setId(123);
+        Book book = new Book("title","publisher",new Date(),"category",20l,true,user,"content");
+        Mockito.when(digitalBookRepository.save(Mockito.any())).thenReturn(book);
+        BookRequest bookRequest = new BookRequest();
+        bookRequest.setBookId(123);
+        BookResponse bookResponse = new BookResponse("title","publisher",new Date(),"category",20l,"author","content",true);
+        bookRequest.setResponse(bookResponse);
+        bookRequest.setUserId(123);
+        Book b = digitalBookService.createBook(bookRequest,123);
+        Assert.assertNotNull(b);
+    }
+
+    @Test(expected = DigitalBookException.class)
+    public void createBookExceptionTest() throws Exception {
+        User user = new User();
+        user.setId(123);
+        BookRequest bookRequest = new BookRequest();
+        bookRequest.setBookId(123);
+        Assert.assertNull(digitalBookService.createBook(bookRequest,123));
+    }
+
+    @Test
+    public void updateBookDetails() throws Exception{
+        User user = new User();
+        user.setId(123);
+        Book book = new Book("title","publisher",new Date(),"category",20l,true,user,"content");
+        Mockito.when(digitalBookRepository.save(Mockito.any())).thenReturn(book);
+        BookRequest bookRequest = new BookRequest();
+        bookRequest.setBookId(123);
+        BookResponse bookResponse = new BookResponse("title","publisher",new Date(),"category",20l,"author","content",true);
+        bookRequest.setResponse(bookResponse);
+        bookRequest.setUserId(123);
+        BookResponse bookResponse1 = digitalBookService.updateeBookDetails(bookRequest,123,123);
+        Assert.assertNotNull(bookResponse1);
+    }
+
+    @Test(expected = DigitalBookException.class)
+    public void updateBookExceptionTest() throws Exception {
+        User user = new User();
+        user.setId(123);
+        BookRequest bookRequest = new BookRequest();
+        bookRequest.setBookId(123);
+        Assert.assertNull(digitalBookService.updateeBookDetails(bookRequest,123,123));
+    }
+
+
+    @Test
+    public void buyBook() throws Exception{
+        PaymentRequest paymentRequest = new PaymentRequest();
+        paymentRequest.setBookId(123);
+        paymentRequest.setUserId(123);
+        User user = new User();
+        user.setId(paymentRequest.getUserId());
+        Book book = new Book("title","publisher",new Date(),"category",20l,true,user,"content");
+        Payment payment = new Payment(new Date(),book,user);
+        Mockito.when(paymentRepository.save(Mockito.any())).thenReturn(payment);
+        Payment p = digitalBookService.buyBook(paymentRequest);
+        Assert.assertNotNull(p);
+    }
+
+    @Test(expected = DigitalBookException.class)
+    public void buyBookException() throws Exception{
+        PaymentRequest paymentRequest = null;
+        digitalBookService.buyBook(paymentRequest);
+    }
+
+    @Test
+    public void getPaymentDetails() {
+        User user = new User();
+        user.setId(123);
+        Book book = new Book("title","publisher",new Date(),"category",20l,true,user,"content");
+        Payment payment = new Payment(new Date(),book,user);
+        List<Payment> paymentList = new ArrayList<>();
+        paymentList.add(payment);
+        Mockito.when(paymentRepository.findByUserUserId(Mockito.anyInt())).thenReturn(paymentList);
+        List<PaymentModel> paymentModels = digitalBookService.getPaymentDetails(123);
+        Assert.assertNotNull(paymentModels);
+    }
+
+    @Test(expected = ResultNotFoundException.class)
+    public void getPaymentDetailsException() {
+        User user = new User();
+        user.setId(123);
+        List<Payment> paymentList = new ArrayList<>();
+        Mockito.when(paymentRepository.findByUserUserId(Mockito.anyInt())).thenReturn(paymentList);
+        List<PaymentModel> paymentModels = digitalBookService.getPaymentDetails(123);
+        Assert.assertNull(paymentModels);
+    }
+
+    @Test
+   public void readContent() {
+        User user = new User();
+        user.setId(123);
+        Book book = new Book("title","publisher",new Date(),"category",20l,true,user,"content");
+        Mockito.when(digitalBookRepository.findById(123)).thenReturn(Optional.of(book));
+        Assert.assertNotNull(digitalBookService.readContent(123));
+    }
+
+    @Test
+   public void getPaymentDetailsById() {
+        User user = new User();
+        user.setId(123);
+        Book book = new Book("title","publisher",new Date(),"category",20l,true,user,"content");
+        Payment payment = new Payment(new Date(),book,user);
+        Mockito.when(paymentRepository.findById(12l)).thenReturn(Optional.of(payment));
+        Assert.assertNotNull(digitalBookService.getPaymentDetailsById(12l));
+    }
 }
