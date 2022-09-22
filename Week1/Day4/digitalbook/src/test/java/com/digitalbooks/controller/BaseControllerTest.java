18bd24edd54d218ff7155b12ac42de630a2afde6
poojanaik7 <naikpooja107@gmail.com>
2022-09-22 19:34:19 +0530

    Handle exception

--- Week1/Day4/digitalbook/src/test/java/com/digitalbooks/controller/BaseControllerTest.java
+++ Week1/Day4/digitalbook/src/test/java/com/digitalbooks/controller/BaseControllerTest.java
@@ -1,4 +1,33 @@
 package com.digitalbooks.controller;
 
+import com.digitalbooks.exception.DigitalBookException;
+import com.digitalbooks.exception.ResultNotFoundException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.sql.SQLException;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BaseControllerTest {
+
+    @InjectMocks
+    BaseController baseController;
+
+    @Test
+    public void handleSqlExceptionTest(){
+        baseController.handleSqlException(new SQLException("SQL Exception"));
+    }
+
+    @Test
+    public void handleExceptionTest(){
+        baseController.handleException(new DigitalBookException("Something went wrong please try again later"));
+    }
+
+    @Test
+    public void handleResultNotFoundExceptionTest(){
+        baseController.handleResultNotFoundException(new ResultNotFoundException("SQL Exception"));
+    }
 }
